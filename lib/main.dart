ef112db43fe903789ee10c06ccb4768f76f1dba3
jungchungsub <cndtjq1248@naver.com>
2023-02-25 16:03:02 +0900

    프로젝트 초기설정

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/post/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //라우터 설계 필요없음, GetX 사용 예정
+      home: HomePage(
+
+      ),
+    );
+  }
+}
