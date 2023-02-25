d5ba6dff537a704d52177afcd2d8a837d890d5b2
jungchungsub <cndtjq1248@naver.com>
2023-02-25 17:07:38 +0900

    로그인, 회원가입페이지 화면구상

---
+++ lib/components/custom_text_form_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String hint;
+
+  const CustomTextFormField({required this.hint});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: TextFormField(
+        obscureText: hint == "Password" ? true : false,
+        decoration: InputDecoration(
+          hintText: "Enter $hint",
+          enabledBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          focusedBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          errorBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          focusedErrorBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/pages/post/home_page.dart';
+import 'package:flutter_blog/pages/user/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //라우터 설계 필요없음, GetX 사용 예정
-      home: HomePage(
-
-      ),
+      home: LoginPage(),
     );
   }
 }
